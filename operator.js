0a45282d2092aa97807e705be16c0a60f44c5819
ashifchauhan <chauhanashif3@gmail.com>
2024-05-23 13:15:35 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,25 @@
+//operator
+//operator
+//operator are used perform some operation on data
+//types of operator
+//1.arithmetic operator
+//this operator used to perform arithmetic operation
+let a=10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(++a);
+console.log(--a);
+//2.assignment operartor
+//this operator used to assign value in varriable
+// =  same as x=y
+// += same as x=x+y
+// -= same as x=x-y
+// *= same as x=x*y
+// /= same as x=x/y
+// **= same as x=x**y
+// %= same as x=x%y
+
